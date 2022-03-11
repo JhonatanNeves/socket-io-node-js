13e766d79ffbbbcdd011d3cb2c8b11c7fc39f577
jhonatanneves <nevesjn2@gmail.com>
2022-03-11 16:53:57 -0300

    first-commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var app = express();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+
+io.on("connection", (socket) => {
+
+    socket.on("boasvindas", (data) => {
+        console.log("Executando evento de boas vindas");
+        console.log(data);
+    })
+    socket.on("palavra", (data) => {
+        console.log(data);
+        socket.emit("resultado", data + " - Gui Do Usuario!");
+    })
+})
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+http.listen(3000, () => {
+    console.log("App Rodando!");
+})
